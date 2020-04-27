f2e57efc6bd6a136f57c87b4c27a9d3c49982c94
djstephenson7 <dan_0502@hotmail.co.uk>
2020-04-27 09:21:45 +0100

    Handle form with React-Hook-Form

--- app/src/screens/SignupScreen.tsx
+++ app/src/screens/SignupScreen.tsx
@@ -1,47 +1,84 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useForm } from 'react-hook-form';
 import {
-  View,
-  Text,
   StyleSheet,
+  Text,
   TextInput,
   TouchableOpacity,
+  View,
 } from 'react-native';
 
-export interface SignupScreenProps {}
+type FormData = {
+  username: string;
+  email: string;
+  firstName: string;
+  surname: string;
+  password: string;
+  confirmPassword: string;
+};
 
-const SignupScreen = () => {
+const SignupScreen = ({ navigation }) => {
+  const onSubmit = (data) => {
+    console.log('Form Data', data);
+  };
+  const { register, handleSubmit, setValue } = useForm<FormData>();
+
+  useEffect(() => {
+    register('username');
+    register('email');
+    register('firstName');
+    register('surname');
+    register('password');
+    register('confirmPassword');
+  }, [register]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.header}>Signup screen</Text>
       <TextInput
+        onChangeText={(text) => setValue('username', text)}
         style={styles.inputText}
         placeholder="Username"
         autoCapitalize="none"
       />
       <TextInput
+        onChangeText={(text) => setValue('email', text)}
         style={styles.inputText}
         placeholder="Email"
         autoCapitalize="none"
       />
-      <TextInput style={styles.inputText} placeholder="First Name" />
-      <TextInput style={styles.inputText} placeholder="Surname" />
       <TextInput
+        onChangeText={(text) => setValue('firstName', text)}
+        style={styles.inputText}
+        placeholder="First Name"
+      />
+      <TextInput
+        onChangeText={(text) => setValue('surname', text)}
+        style={styles.inputText}
+        placeholder="Surname"
+      />
+      <TextInput
+        onChangeText={(text) => setValue('password', text)}
         style={styles.inputText}
         placeholder="Password"
         secureTextEntry
         autoCapitalize="none"
       />
       <TextInput
+        onChangeText={(text) => setValue('confirmPassword', text)}
         style={styles.inputText}
         placeholder="Confirm Password"
         secureTextEntry
         autoCapitalize="none"
       />
 
-      <TouchableOpacity>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
         <Text style={styles.loginText}>To Login Screen</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={styles.loginBtn}>
+      <TouchableOpacity
+        onPress={handleSubmit(onSubmit)}
+        style={styles.loginBtn}
+      >
         <Text style={styles.loginText}>SIGNUP</Text>
       </TouchableOpacity>
     </View>
@@ -53,9 +90,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#5F9EA0',
     alignItems: 'center',
-    justifyContent: 'center',
   },
   header: {
+    marginTop: '10%',
     marginBottom: '5%',
     fontSize: 30,
   },
